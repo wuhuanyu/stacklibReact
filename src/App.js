92b85c81815913eb8b5dcf13df1912864314f83d
吴环宇 <1790949749@qq.com>
2017-08-16 20:34:54 +0800

    add circular-progress hint

--- src/App.js
+++ src/App.js
@@ -1,43 +1,77 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Guide from './components/Guide';
 import logo from './logo.svg';
 import './App.css';
 import ScrollableTab from './components/ScrollBar'
 import MockRep from './repository/MockRep';
-import Header from  './components/CommonItems';
+import Header from './components/CommonItems';
 import pic from './repository/mockPic.jpg';
 import {NewsList} from "./components/CommonItems"
+import {CircularProgress} from 'material-ui/Progress';
+
+import {withStyles} from 'material-ui/styles';
+
+const styles = theme => ({
+    progress: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translateX(-50%) translateY(-50%)'
+    }
+})
+
 class App extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            // cardData:MockRep.withId().then((data)=>data),
-            currentTab:'BBC',
-            sources:['BBC','REUTERS','MEDIUM'],
+        this.state = {
+            currentTab: 'BBC',
+            sources: [
+                'BBC', 'REUTERS', 'MEDIUM'
+            ],
+            isFetching: true
         };
 
-        this.handleSwitchTag=this.handleSwitchTag.bind(this);
+        this.handleSwitchTag = this
+            .handleSwitchTag
+            .bind(this);
     }
 
-    handleSwitchTag(e,index){
-        console.log('app handleSwitchTag',index);
+    handleSwitchTag(e, index) {
+        console.log('app handleSwitchTag', index);
+        this.setState({currentTab: this.state.sources[index]});
+    }
+
+    toggleisFetching(fetched) {
+        console.log('data is fetched? ')
+        console.log(fetched);
         this.setState({
-            currentTab:this.state.sources[index],
-        });
+            isFetching: !fetched
+        })
     }
-    componentDidMount(){
-    }
+    componentDidMount() {}
     render() {
-        const state= this.state;
+        const state = this.state;
+        let {isFetching} = this.state;
+        const {classes} = this.props;
+        let circularProgress = isFetching
+            ? <CircularProgress className={classes.progress}/>
+            : null;
+        // let guide = isFetching? null:<Guide source={state.currentTab}
+        // fetchedHandler={this.toggleisFetching}/>;
         return (
             <div>
                 <ScrollableTab sources={state.sources} handleChange={this.handleSwitchTag}/>
-                <Guide source={state.currentTab}/>
-                {/*<Header title={card.title} summary={card.summary} img={pic}/>*/}
-                {/*<NewsList newss={MockRep.withField(3,['summary','title','_id','image_urls'])}/>*/}
+                <Guide
+                    source={state.currentTab}
+                    fetchedHandler={this
+                    .toggleisFetching
+                    .bind(this)}/> 
+                    
+                {circularProgress}
             </div>
+
         )
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,39 +1,52 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Header, {NewsList,ListCard} from "./CommonItems";
+import Header, {NewsList, ListCard} from "./CommonItems";
 import MockRep from '../repository/MockRep';
 import noPic from '../repository/nopic.jpg';
 
-class Guide extends Component{
-    constructor(props){
+class Guide extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            headerData:'',
-            listData:'',
+        this.state = {
+            headerData: '',
+            listData: ''
         }
     }
 
-    componentDidMount(){
-        MockRep.withId('da').then(data=>this.setState({headerData:data}));
-        MockRep.withField(3,['title','_id','image_urls','summary']).then(data=>this.setState({listData:data}));
+    componentDidMount() {
+        let {fetchedHandler} = this.props;
+        MockRep
+            .withId('da')
+            .then(data => {
+                console.log(data);
+                this.setState({headerData: data})
+            });
+        MockRep
+            .withField(3, ['title', '_id', 'image_urls', 'summary'])
+            .then(data => {
+                fetchedHandler(true);
+                this.setState({listData: data})
+            });
     }
 
-
-    render(){
+    render() {
         const props = this.props;
-        const state=this.state;
-        return(
+        const state = this.state;
+        return (
             <div>
-                <Header id={state.headerData&&state.headerData._id||''} title={state.headerData&&state.headerData.title||''} summary={state.headerData.summary} img={state.headerData&&state.headerData.image_urls[0]||noPic}/>
-                <NewsList newss={state.listData&&state.listData||[]}/>
+                <Header
+                    id={state.headerData && state.headerData._id || ''}
+                    title={state.headerData && state.headerData.title || ''}
+                    summary={state.headerData.summary}
+                    img={state.headerData && state.headerData.image_urls[0] || noPic}/>
+                <NewsList newss={state.listData && state.listData || []}/>
             </div>
         )
     }
 }
 
-Guide.propTypes={
-    source:PropTypes.string.isRequired,
+Guide.propTypes = {
+    source: PropTypes.string.isRequired
 };
 
 export default Guide;
-
