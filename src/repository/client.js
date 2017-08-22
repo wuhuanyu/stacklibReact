063bc99b2af22f508db9f6f4da72e5a68fdf6245
wuhuanyu <1790949749@qq.com>
2017-08-22 13:29:39 +0800

    client#2

--- src/repository/client.js
+++ src/repository/client.js
@@ -1,43 +1,65 @@
 const host = "localhost:3001";
-const url = "/api/v1";
+const url = "/api/v1/";
 const defaultDomain = host + url;
 const defaultHeaders = {
     method: 'GET',
     cache: 'default'
 }
 
-const constructRecentNewsUrl = (source, tag, count=5, fields) => {
-   let  url = "http://"+host+url+"/"+source+"/recent?";
-   let _tag =tag?"tag="+tag:null;
-   let _fields=fields? "fields="+fields.join(','):null;
-   let _count="count="+count;
+const constructRecentNewsUrl = (source, tag, count = 5, fields) => {
+    let url = "http://" + defaultDomain + source + "/recent?";
+    let _tag = tag ? "tag=" + tag + "&" : '';
+    let _fields = fields ? "fields=" + fields.join(',') + '&' : '';
+    let _count = "count=" + count;
 
-   url = url +[_tag,_fields,_count].join('&');
-   console.log('[constructRecentNewsUrl ] url='+url);
-   return url;
+    url = url + _tag + _fields + _count;
+    console.log('[constructRecentNewsUrl ] url=' + url);
+    return url;
+};
+const constructIdNewsUrl = (source, id, fields) => {
+    let url = "http://" + defaultDomain + source + "/id-" + id + '?';
+    let _fields = fields ? "fields=" + fields.join(',') : '';
+    url = url + fields;
+    console.log('[constructIdNewsUrl] url=' + url);
+    return url;
+};
+
+const constructTagNewsUrl = (source, tag, count = 5, fields) => {
+    let url = "http://" + defaultDomain + source + "/tag-" + tag + '?';
+    let _fields = fields ? "fields=" + fields.join(',') + '&' : '';
+    let _count = "count=" + count;
+    url = url + _fields + count;
+    console.log('[constructTagNewsUrl] url=' + url);
+    return url;
 }
 
 
 window.client = (function () {
 
-    let getNewsRecent = function (source, tag, count=5, fields) {
-        return fetch(constructRecentNewsUrl(source,tag,count,fields),defaultHeaders);
+    let getNewsRecent = function (source, tag, count = 5, fields) {
+        return fetch(constructRecentNewsUrl(source, tag, count, fields), defaultHeaders);
     };
 
-    let getNewsById=(source,id,fields){
+    let getNewsById = (source, id, fields) => {
+        return fetch(constructIdNewsUrl(source, id, fields), defaultHeaders);
+    }
+
+    let getNewsByTag = (source, tag, count = 5, fields) => {
+        return fetch(constructTagNewsUrl(source, tag, count, fields), defaultHeaders);
 
     }
-    
-    let getNewsByTag=(source,tag,count=5,fields){
+
+    let getMediumById=(id,fields)=>{
+
+    }
+    let getMediumRecent=function(count=5,fields){
 
     }
 
     return {
         getNewsRecent,
         getNewsById,
+        getNewsByTag,
     }
-
-
-
 
 })();
