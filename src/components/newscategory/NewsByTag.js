3f1141711fb00d016769346626f6b5f53cfc92dd
wuhuanyu <1790949749@qq.com>
2017-08-22 20:35:45 +0800

    ui#3

--- src/components/newscategory/NewsByTag.js
+++ src/components/newscategory/NewsByTag.js
@@ -1,24 +1,47 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {withStyles} from 'material-ui/styles';
+import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
-import List, {ListItem} from 'material-ui/List';
-import {CardContent, CardMedia} from 'material-ui/Card';
+import List, { ListItem } from 'material-ui/List';
+import { CardContent, CardMedia } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import NewsListItem from './NewsListItem';
-import {mockClient} from '../../repository/client';
+import { mockClient } from '../../repository/client';
 
-class ListCard extends Component{
-    constructor(props){
-       super(props);
+class ListCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        }
     }
 
-    
+    componentDidMount() {
+        const _this = this;
+        let { source, tag } = this.props;
+        mockClient.getNewsRecent(source, tag, 3, ['_id', 'title', 'image_urls'])
+            .then(data => {
+                console.log('[ListCard] feteched data:'+data);
+                _this.setState({
+                    data: data.data
+                })
+            })
+    }
+
+    render(){
+
+        let {data}=this.state;
+        // if(data.length==0){
+        //     return null;
+        // }
+        
+    }
 }
 
 
-ListCard.PropTypes={
-    
+ListCard.PropTypes = {
+    source: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
 }
 // const ListCard = (props) => {
 //     const classes = props.classes;
@@ -27,7 +50,7 @@ ListCard.PropTypes={
 //         let items = data.map(d=><NewsListItem title={d.title} img={d.image_urls[0]} id={d._id}/>);
 
 //         return (
-            
+
 //             <div>
 //                 <List>
 //                     {items}
