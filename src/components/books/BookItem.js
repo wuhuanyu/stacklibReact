e7b24a13b1b2db944290a8dcdf3070c5aa0b73d2
吴环宇 <1790949749@qq.com>
2017-08-29 12:45:23 +0800

    book#6

--- src/components/books/BookItem.js
+++ src/components/books/BookItem.js
@@ -5,55 +5,70 @@ import {ListItem} from 'material-ui/List';
 import Typography from 'material-ui/Typography';
 import {withStyles} from 'material-ui/styles';
 import Divider from 'material-ui/Divider';
-const CSS = theme=>({
-    root:{
-        display:'flex',
+const CSS = theme => ({
+    root: {
+        display: 'flex',
+        // height:200,
+        // marginTop:'5px',
+        marginBottom:'5px',
     },
 
-    image:{
-        width:'40%',
-        height:'auto',
-        // position:'relative'
-    },
-    image_content:{
-        width:'100%',
-        height:'auto',
-    },
-
-    detail:{
-        width:'60%',
+    imageContainer: {
+        width: '40%',
+        height: 'auto',
         display:'flex',
         flexDirection:'column',
+        justifyContent:'center',
     },
-    title:{
-        flex:1
+    image: {
+        width: '100%',
+        height: 'auto',
+
     },
 
-    summary:{
-        flex:4
+    detail: {
+        width: '60%',
+        // height:'200',
+        flexDirection: 'column'
+    },
+    title: {
+        flexGrow: 1
+    },
+
+    summary: {
+        flexGrow: 5
     }
 
 });
 
-
-
-const BookItem = ({_id, crawled_at, title, img_url, summary,classes}) => {
+const BookItem = ({
+    _id,
+    crawled_at,
+    title,
+    img_url,
+    summary,
+    classes
+}) => {
     return (
         <div>
-                <Card className={classes.root}>
-                    <CardMedia className={classes.image}>
-                        <img src={img_url && img_url[0]} alt={"Eh,Something happened"} className={classes.image_content}/>
-                    </CardMedia>
-                    <CardContent className={classes.detail}>
-                        <Typography className={classes.title}>
-                            {title}
-                        </Typography>
-                        <Divider/>
-                        <Typography className={classes.summary}>
-                            {summary}
-                        </Typography>
-                    </CardContent>
-                </Card>
+            <Card className={classes.root}>
+                <CardMedia className={classes.imageContainer}>
+                    <img
+                        src={img_url && img_url[0]}
+                        alt={"Eh,Something happened"}
+                        className={classes.image}/>
+                </CardMedia>
+                <CardContent className={classes.detail}>
+                    <Typography className={classes.title} type="subheading">
+                        {title}
+                    </Typography>
+                    <Divider/>
+                    <Typography className={classes.summary}>
+                        {summary}
+                        {/* {summary} */}
+                    </Typography>
+                </CardContent>
+            </Card>
         </div>
     )
 };
@@ -64,7 +79,7 @@ BookItem.PropType = {
     title: PropType.string.isRequired,
     img_url: PropType.array.isRequired,
     summary: PropType.array.isRequired,
-    classes:PropType.object.isRequired,
+    classes: PropType.object.isRequired
 }
 
 export default withStyles(CSS)(BookItem);
