4a25b96c9be708068794066a64e907eca622e408
吴环宇 <1790949749@qq.com>
2017-09-01 12:19:24 +0800

    article#4

--- src/components/news/Article.js
+++ src/components/news/Article.js
@@ -19,11 +19,10 @@ import MenuIcon from 'material-ui-icons/Menu';
 import {mockClient} from '../../repository/client';
 import {AllNewsFields} from '../../constants/Constants';
 import {num2Time} from '../../utility/Utils';
+import Chip from 'materia'
 const CSS = theme => ({
     root: {
         marginTop: '5px',
-        marginLeft: '2px',
-        marginRight: '2px',
         position: 'relative',
         height: '100%'
     },
@@ -31,12 +30,15 @@ const CSS = theme => ({
         marginTop: '3px',
         marginLeft: '3px'
     },
-    imgContainer: {
+    summary: {
+        marginLeft: '5px',
+        marginRight:'5px',
     },
+    imgContainer: {},
     img: {
         width: '100%',
         height: 'auto',
-        borderRadius:'5px'
+        borderRadius: '5px'
     }
 })
 
@@ -44,16 +46,16 @@ const Text = ({t}) => (
     <Typography
         paragraph
         style={{
-        marginLeft: '3px',
-        marginRight: '3px'
+        marginLeft: '5px',
+        marginRight: '5px'
     }}>{t}</Typography>
 )
 
 class Article extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            data:{}
+        this.state = {
+            data: {}
         }
     }
 
@@ -75,16 +77,14 @@ class Article extends Component {
             timestamp,
             url,
             summary,
-            _id,
+            _id
         } = this.state.data;
 
         let texts = null;
         if (text) {
-            texts = 
-                text
-                .map((t, idx) => {
-                    return <Text key={idx +_id} t={t}/>
-                })
+            texts = text.map((t, idx) => {
+                return <Text key={idx + _id} t={t}/>
+            })
         }
         return (
             <div>
@@ -92,13 +92,21 @@ class Article extends Component {
                     position="static"
                     color="default"
                     style={{
-                    width: '100%',display:'flex',alignContent:'center'
+                    width: '100%',
+                    display: 'flex',
+                    alignContent: 'center'
                 }}>
                     <Toolbar disableGutters>
                         <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
                             <MenuIcon/>
                         </IconButton>
-                        <Typography type="title" color="inherit" className={classes.flex} style={{flex:1}}>
+                        <Typography
+                            type="title"
+                            color="inherit"
+                            className={classes.flex}
+                            style={{
+                            flex: 1
+                        }}>
                             {"News"}
                         </Typography>
                         <IconButton>
@@ -114,23 +122,31 @@ class Article extends Component {
                         className={classes.title}>
                         {title}
                     </Typography>
-                    <Typography
+                    <div
                         style={{
-                        float: 'right'
-                    }}
-                        type="caption"
-                        gutterBottom>
-                        {num2Time(timestamp)}
-                    </Typography>
+                        display: 'flex',
+                        flexDirection: 'row-reverse',
+                        alignItems: 'center'
+                    }}>
+                        <Typography
+                            style={{
+                            marginRight: '6px'
+                        }}
+                            gutterBottom>
+                            {num2Time(timestamp)}
+                        </Typography>
+                    </div>
 
                     <Typography
-                        style={{marginRight:'3px',marginLeft:'3px'}}
+                       className={classes.summary}
                         type="subheading"
                         gutterBottom
                         style={{
                         clear: 'right'
                     }}>
-                        {"Summary: " + summary}
+                        {summary
+                            ? "Summary: " + summary
+                            : null}
                     </Typography>
                     <div className={classes.imgContainer}>
                         <img src={image_urls && image_urls[0]} className={classes.img}/>
@@ -144,31 +160,27 @@ class Article extends Component {
 }
 
 // const Article = ({article, source, tag, classes}) => {     let texts = null;
-//    if (article.text) {         texts = article             .text
-// .map((t, idx) => {                 return <Text key={idx + article._id}
-// t={t}/>             })     }     return (         <div>             <AppBar
-// position="static" style={{                 width: '100%'             }}>
-//            <Toolbar disableGutters>                     <IconButton
-// className={classes.menuButton} color="contrast" aria-label="Menu">
-//              <MenuIcon/>                     </IconButton>
-//  <Typography type="title" color="inherit" className={classes.flex}>
-//               {"News"}                     </Typography>
-// <Button color="contrast">Share</Button>                 </Toolbar>
-//  </AppBar>             <Paper elevation={2} className={classes.root}>
-//         <Typography                     type="headline"
-// component="h3"                     gutterBottom
-// className={classes.title}>                     {article.title}
-//  </Typography>                 <Typography                     style={{
-//               float: 'right'                 }}
-// type="caption"                     gutterBottom>
-// {article.timestamp}                 </Typography>                 <Typography
-//                     type="subheading"                     gutterBottom
-//              style={{                     clear: 'right'                 }}>
-//                    {"Summary: " + article.summary}
-// </Typography>                 <div className={classes.imgContainer}>
-//            <img src={article.image_urls && article.image_urls[0]}
-// className={classes.img}/>                 </div>                 {texts}
-//        </Paper>         </div>     ) }
+//  if (article.text) {         texts = article             .text .map((t, idx)
+// => {                 return <Text key={idx + article._id} t={t}/> })     }
+//  return (         <div>             <AppBar position="static" style={{
+//          width: '100%'             }}>            <Toolbar disableGutters>
+//                  <IconButton className={classes.menuButton} color="contrast"
+// aria-label="Menu">   <MenuIcon/>                     </IconButton>
+// <Typography type="title" color="inherit" className={classes.flex}>
+//    {"News"}        </Typography> <Button color="contrast">Share</Button>
+// </Toolbar>  </AppBar>             <Paper elevation={2}
+// className={classes.root}>         <Typography type="headline" component="h3"
+//                    gutterBottom className={classes.title}>
+//  {article.title}  </Typography>                 <Typography
+//   style={{               float: 'right'                 }} type="caption"
+//                 gutterBottom> {article.timestamp}
+// </Typography>                 <Typography
+// type="subheading"                     gutterBottom       style={{
+//         clear: 'right'                 }}>            {"Summary: " +
+// article.summary} </Typography>                 <div
+// className={classes.imgContainer}>            <img src={article.image_urls &&
+// article.image_urls[0]} className={classes.img}/>                 </div>
+//     {texts}        </Paper>         </div>     ) }
 
 Article.PropTypes = {
     source: PropTypes.string.isRequired,
