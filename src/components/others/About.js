122ecc60667a6c6fca58c5dffda1c730bdda63f7
wuhuanyu <1790949749@qq.com>
2017-09-02 18:04:09 +0800

    feedback#done

---
+++ src/components/others/About.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Typography from 'material-ui/Typography';
+import withAppBar from './withAppBar';
+import Paper from 'material-ui/Paper';
+
+
+
+const About = (props) => {
+    return (
+        <div style={{ marginTop: '3px', width: '100%' }}>
+            <Paper style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                <div style={{marginLeft:'3px',marginRight:'3px'}}>
+                <Typography style={{ display: 'flex', justifyContent: 'center', marginTop:"5px"}} type="title" gutterBottom>
+                    {"关于"}
+                </Typography>
+                {
+                    [
+                        "这个简单的demo想要传达的就是分享和发现的精神",
+                        "长期以来，我们都生活在官方的话语背景之下",
+                        "我们对于世界的认知还是太局限了",
+                        "就拿语言学习来讲，很多人也包括我，学习的仅仅是功能性语言，会很多单词，听说读写样样精通，但是这掩盖不了人文的缺失",
+                        "一个民族的语言是认识它的窗口，历史、文化、思维方式、甚至哲学等等，这才是深层次的认知",
+                        "在我设想中，这个demo将会把外国的优秀文化，人文精神呈现在我们面前",
+                        "他们的审美，思考方式，甚至世界观等等",
+                        "尽管目前内容还很少，很单一",
+                        "顺便挑战一下自己",
+                        "内容来自互联网，无利益瓜葛,建设性意见和建议，尽可通过Feedback，发送给我"
+                    ].map((t, idx) => {
+                        return (<Typography type="paragraph" style={{}} key={idx}>
+                            {t}
+                        </Typography>)
+                    })
+                }
+                </div>
+            </Paper>
+        </div>
+    )
+};
+
+
+
+export default withAppBar(About,"关于");
--- src/components/others/Feedback.js
+++ src/components/others/Feedback.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import {findDOMNode} from 'react-dom';
+import React, { Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import PropTypes from 'prop-types';
 import Typography from 'material-ui/Typography';
 import TextField from 'material-ui/TextField';
 import withAppBar from './withAppBar';
 import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
-import Menu, {MenuItem} from 'material-ui/Menu';
+import Menu, { MenuItem } from 'material-ui/Menu';
 
 const postApi = "";
 
@@ -16,8 +16,9 @@ class Feedback extends Component {
         this.state = {
             open: false,
             el: undefined,
-            category:'',
-            text:'',
+            category: '',
+            text: '',
+            cT: 0,
         }
     }
 
@@ -27,13 +28,31 @@ class Feedback extends Component {
         })
     }
 
-    closeMenu(){
+    closeMenu() {
+        this.setState({
+            open:false,
+        })
+    }
+    openMenu(){
+        this.setState({
+            open:true
+        })
 
     }
     onCategoryClick() {
         this.setState({
-
         })
+    }
+
+    onCategoryItemClick(idx) {
+        this.setState({
+            cT: idx,
+            open:false,
+        })
+    }
+    
+    submitFB(){
+
     }
     render() {
         return (
@@ -43,21 +62,29 @@ class Feedback extends Component {
                         type="title"
                         gutterBottom
                         style={{
-                        display: 'flex',
-                        justifyContent: 'center'
-                    }}>
+                            display: 'flex',
+                            justifyContent: 'center'
+                        }}>
                         {"来，工作人员递上话筒"}
                     </Typography>
                     <form noValidate>
                         <Button
                             ref={node => this.categoryAnchor = node}
                             onClick={this
-                            .onCategoryClick
-                            .bind(this)}>{"tucao"}</Button>
-                        <Menu open={this.state.open} anchorEl={this.state.el} onRequestClose={this.onCategoryClick.bind(this)}>
-
+                                .onCategoryClick
+                                .bind(this)}>{"tucao"}</Button>
+                        <Menu open={this.state.open} anchorEl={this.state.el} onRequestClose={this.closeMenu.bind(this)}>
+                            {
+                                this.props.categories.map((cat, idx) => {
+                                    return <MenuItem key={cat} selected={idx === this.state.cT} onClick={event => this.onCategoryItemClick(idx)} />
+                                })
+                            }
                         </Menu>
-                        <TextField id={"category"} label={"Text"}/>
+                        <TextField id={"category"} label={"Text"} multiLine />
+                        <Button onClick={this.submitFB.bind(this)}>
+                            {"Submit"}
+                        </Button>
+
                     </form>
                 </Paper>
             </div>
@@ -65,8 +92,8 @@ class Feedback extends Component {
     }
 }
 
-Feedback.PropTypes={
-    
+Feedback.PropTypes = {
+    categories: PropTypes.array.isRequired,
 }
 
 
